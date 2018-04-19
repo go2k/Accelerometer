55802932d325ab194950f56f87de0055d3fda1b9
Michael König <m.koenig@go2k.de>
2018-04-19 10:19:34 +0200

    mit sound

--- app/src/main/java/com/example/leon/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/leon/accelerometer/MainActivity.java
@@ -6,12 +6,16 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -22,12 +26,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     float now = SensorManager.GRAVITY_EARTH;
     float last = SensorManager.GRAVITY_EARTH;
 
+    MediaPlayer mediaPlayer;
+    String AudioSavePathInDevice = "";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+            }
+        });
 
+
+        AudioSavePathInDevice = Environment.getExternalStorageDirectory().getAbsolutePath().toString() + "/" + "Audiorecording.3gp";
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
@@ -37,9 +53,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         xMaxText = findViewById(R.id.xMaxText);
         yMaxText = findViewById(R.id.yMaxText);
         zMaxText = findViewById(R.id.zMaxText);
-
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-
     }
 
 
@@ -59,15 +73,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         now = (float) Math.sqrt(x * x + y * y + z * z);
 
         float grenzWert = now - last;
-        if (grenzWert > 12) {
-            Toast.makeText(MainActivity.this, "langsamer", Toast.LENGTH_LONG).show();
+
+        if (grenzWert > 0) {
+           // Toast.makeText(MainActivity.this, "langsamer", Toast.LENGTH_LONG).show();
             xMaxText.setText("X-Max: " + event.values[0]);
             yMaxText.setText("Y-Max: " + event.values[1]);
             zMaxText.setText("Z-Max: " + event.values[2]);
 
-        }
+            try {
+                mediaPlayer.setDataSource(AudioSavePathInDevice);
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+                Toast.makeText(MainActivity.this, "playing", Toast.LENGTH_LONG).show();
 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
